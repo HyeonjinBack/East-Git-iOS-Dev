956f9ffea6af7bad6cab26fdd596a2c63b7cf343
백현진 <guswlsrlacl@naver.com>
2025-05-10 16:47:35 +0900

    박현준님 MBTI, 강점 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -243,8 +243,8 @@ let people: [SelfIntroduction] = [
         name: "박현준",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
         hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
+        mbti: "INFP",
+        strength: "머리가 좋다",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
         job: "카카오 - iOS 개발자"
